3dc504bc1f2db890a55719f8ce417ab3f882151a
Tallal <mmtallal@gmail.com>
2024-04-18 00:04:27 +0500

    Start Page

--- app/src/main/java/com/tallaleatazaz/ta_ldmanagementsystem/MainActivity.kt
+++ app/src/main/java/com/tallaleatazaz/ta_ldmanagementsystem/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.tallaleatazaz.ta_ldmanagementsystem
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val handler = Handler()
+    private val navigateToLogin = Runnable {
+        val intent = Intent(this@MainActivity, Login::class.java)
+        startActivity(intent)
+        finish()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        handler.postDelayed(navigateToLogin, 5000) // Delay in milliseconds (5 seconds)
+    }
+
+    override fun onDestroy() {
+        handler.removeCallbacks(navigateToLogin)
+        super.onDestroy()
+    }
+
+    fun onActivityClicked(view: View) {
+        handler.removeCallbacks(navigateToLogin)
+        navigateToLogin.run()
     }
 }
+
