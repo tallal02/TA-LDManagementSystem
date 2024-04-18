0c04b80a3ae0be33899a1af80b2873394ed330eb
Tallal <mmtallal@gmail.com>
2024-04-18 20:21:06 +0500

    Assigns Tasks done(Dayyan)

---
+++ app/src/main/java/com/tallaleatazaz/ta_ldmanagementsystem/AssignTasks.kt
@@ -0,0 +1,11 @@
+package com.tallaleatazaz.ta_ldmanagementsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AssignTasks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_assign_tasks)
+    }
+}
