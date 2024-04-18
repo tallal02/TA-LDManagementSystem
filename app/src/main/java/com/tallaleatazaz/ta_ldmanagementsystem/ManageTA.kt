300df61d1b6b60606d7a9059a68bd50adf834feb
Tallal <mmtallal@gmail.com>
2024-04-18 20:17:41 +0500

    Manage TA done(Dayyan)

---
+++ app/src/main/java/com/tallaleatazaz/ta_ldmanagementsystem/ManageTA.kt
@@ -0,0 +1,11 @@
+package com.tallaleatazaz.ta_ldmanagementsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ManageTA : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manage_ta)
+    }
+}
---
+++ app/src/main/java/com/tallaleatazaz/ta_ldmanagementsystem/ViewProfile.kt
@@ -0,0 +1,11 @@
+package com.tallaleatazaz.ta_ldmanagementsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_profile)
+    }
+}
