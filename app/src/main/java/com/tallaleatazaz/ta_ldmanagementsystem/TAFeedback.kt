3ed24f02a52c050d4fd2915d41ecfad4824ed078
Tallal <mmtallal@gmail.com>
2024-04-18 23:15:46 +0500

    TA Feedback page

--- app/src/main/java/com/tallaleatazaz/ta_ldmanagementsystem/TAFeedback.kt
+++ app/src/main/java/com/tallaleatazaz/ta_ldmanagementsystem/TAFeedback.kt
@@ -2,12 +2,10 @@ package com.tallaleatazaz.ta_ldmanagementsystem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
-
-class FacultyHomePage : AppCompatActivity() {
+class TAFeedback : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_faculty_home_page)
+        setContentView(R.layout.activity_tafeedback)
     }
 }
