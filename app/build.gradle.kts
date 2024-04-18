e13b7dd24645155fcc089d5b643cd14bd082c891
Tallal <mmtallal@gmail.com>
2024-04-18 20:17:10 +0500

    Manage TA done(Dayyan)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,8 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("com.android.support:design:26.1.0")
+    implementation ("com.google.android.material:material:1.11.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
