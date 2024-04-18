71bd01f76a96087247b60db2a86da0f070ab5c2a
Tallal <mmtallal@gmail.com>
2024-04-18 20:18:43 +0500

    Student Homepage done(Dayyan)

---
+++ app/src/main/java/com/tallaleatazaz/ta_ldmanagementsystem/StudentHome.kt
@@ -0,0 +1,11 @@
+package com.tallaleatazaz.ta_ldmanagementsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StudentHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_home)
+    }
+}
